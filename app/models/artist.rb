8c5b3b7a15884a6564e0ed07f8d9e04228870324
Kayla Cross <kac288@georgetown.edu>
2018-09-10 18:58:16 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -7,7 +7,7 @@ class Artist < ActiveRecord::Base
   end
 
   def self.find_by_slug(slug)
-    self.find_by(name: slug.split('-').join(' ').titleize)
+    self.all.find {|artist| artist.slug == slug}
   end
 
 end
