ad00e46b81fbea1d2b99ac502bb6f1b8ebe15dcb
Kayla Cross <kac288@georgetown.edu>
2018-09-10 19:50:39 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,6 +24,7 @@ class SongsController < ApplicationController
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @song.name = params[:song][:name]
+    @song.artist = Artist.find_or_create_by(name: params[:song][:artist])
   end
 
   post '/songs' do
