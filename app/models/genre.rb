99233c779c28280902b9d60d05a7aec63d18475a
Kayla Cross <kac288@georgetown.edu>
2018-09-10 18:18:43 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,13 @@ class Genre < ActiveRecord::Base
   has_many :song_genres
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
+
+  def slug
+    self.name.split(' ').join('-').downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.find_by(name: slug.split('-').join(' ').titleize)
+  end
+
 end
