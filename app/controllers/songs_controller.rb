b86cd3e52bcd4bf044378c82b492fd115c8abd3c
Kayla Cross <kac288@georgetown.edu>
2018-09-10 19:07:15 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,10 @@ class SongsController < ApplicationController
     erb :"/songs/index"
   end
 
+  get '/songs/new' do
+    
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :"/songs/show"
