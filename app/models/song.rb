e14978e77065912db4a121c759b4b5510584ff23
Kayla Cross <kac288@georgetown.edu>
2018-09-10 18:18:30 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    self.name.split(' ').join('-').downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.find_by(name: slug.split('-').join(' ').titleize)
+  end
+  
 end
