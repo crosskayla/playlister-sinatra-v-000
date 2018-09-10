30d8eded23ea18c341b5e92a4ee463d21b587676
Kayla Cross <kac288@georgetown.edu>
2018-09-10 18:45:16 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,7 +7,7 @@ class SongsController < ApplicationController
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     if @song
-      erb :"songs/show"
+      erb :"/songs/show"
     end
   end
 
